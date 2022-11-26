87a73c924f4b7a0d0f9ec929e85b722fb78a813f
Louis <sorrenti.l@northeastern.edu>
2022-11-25 23:18:16 -0500

    Implemented simple routing -> Louis

---
+++ src/coffee/Homepage/index.js
@@ -0,0 +1,7 @@
+const HomeComponent = () => {
+    return (
+        <h1>Homepage</h1>
+    );
+}
+
+export default HomeComponent;
---
+++ src/coffee/index.js
@@ -0,0 +1,27 @@
+import HomeComponent from "./Homepage";
+import ProfileComponent from "./Profile";
+import LoginComponent from "./Login";
+import {Routes, Route} from "react-router";
+import { configureStore } from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+
+const store = configureStore({reducer: {}});
+
+function Coffee() {
+    return(
+        <Provider store={store}>
+            <div>
+                <Routes>
+                    <Route index element={<HomeComponent/>}/>
+                    <Route path="home"
+                           element={<HomeComponent/>}/>
+                    <Route path="profile"
+                           element={<ProfileComponent/>}/>
+                    <Route path="login"
+                           element={<LoginComponent/>}/>
+                </Routes>
+            </div>
+        </Provider>
+    );
+}
+export default Coffee
