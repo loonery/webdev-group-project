9ce270eaef8e4b51cd0e7006785d8568075bbe0b
Ryan <ryanlooney58@gmail.com>
2022-12-04 10:30:51 -0500

    recipe journal component refactoring

--- src/components/domain-components/recipe-components/RecipeJournal/Comments/index.js
+++ src/components/domain-components/recipe-components/RecipeJournal/Comments/index.js
@@ -1,34 +1,17 @@
-const Comments = () => {
-
+const CommentsSection = () => {
 
     return(
         // the card body houses the comments
-        <div className="card-body border-bottom border-top">
-            <div className="ps-2">Comments</div>
+        <div>
+            <ul className={"pt-1 list-group"}>
+                <div className="card-body border-bottom border-top">
+                    <div className="ps-2">Comments</div>
 
-            {/* comments list component */}
-            <div>
-                <ul className={"pt-1 list-group"}>
-                    <li className="list-group-item border-0">
-                        <div className="d-flex">
-                            <div className="mt-1">
-                                <img className="rounded-circle"
-                                     height="40"
-                                     width={"40"}
-                                     src={imageSource}/>
-                            </div>
+                    {/*todo: map each comment object to a comment item here*/}
 
-                            <div className="flex-grow-1 ms-3 form-group">
-                                {/*user information in the card*/}
-                                <div>{userName} - <span className="fs-6 text-secondary">{date}</span></div>
-                                {/*user post text*/}
-                                <p>{postText}</p>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+                </div>
+            </ul>
         </div>
     );
 }
-export default Comments;
+export default CommentsSection;
