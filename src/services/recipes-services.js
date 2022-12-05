590bbde0f3b8da6942805841df45062b76264bb4
Ryan <ryanlooney58@gmail.com>
2022-12-05 11:59:03 -0500

    adding services for recipes

--- src/services/recipes-services.js
+++ src/services/recipes-services.js
@@ -1,26 +1,26 @@
-// import axios from 'axios';
-//
-// // TUITS_API is a constant referring to our middle tier
-// const API_BASE = process.env.REACT_APP_API_BASE;
-// const TUITS_API = `${API_BASE}/tuits`;
-//
-// // the asynchronous functions take advantage of multi-threading capability
-// export const createTuit = async (tuit) => {
-//     const response = await axios.post(TUITS_API, tuit)
-//     return response.data;
-// }
-// export const findTuits = async () => {
-//     const response = await axios.get(TUITS_API);
-//     const tuits = response.data;    // data in this response is the tuits array
-//     return tuits;                   // return the tuits array
-// }
-//
-// export const deleteTuit = async (tid) => {
-//     const response = await axios.delete(`${TUITS_API}/${tid}`)
-//     return response.data
-// }
-//
-// export const updateTuit = async (tuit) => {
-//     const response = await axios.put(`${TUITS_API}/${tuit._id}`, tuit)
-//     return tuit;
-// }
+import axios from 'axios';
+// TUITS_API is a constant referring to our middle tier
+const API_BASE = process.env.REACT_APP_API_BASE;
+const RECIPE_API = `${API_BASE}/recipes`;
+
+// the asynchronous functions take advantage of multi-threading capability
+export const createRecipe = async (recipe) => {
+    const response = await axios.post(RECIPE_API, recipe)
+    return response.data;
+}
+
+export const findRecipe = async () => {
+    const response = await axios.get(RECIPE_API);
+    const tuits = response.data;    // data in this response is the tuits array
+    return tuits;                   // return the tuits array
+}
+
+export const deleteRecipe = async (rid) => {
+    const response = await axios.delete(`${RECIPE_API}/${rid}`)
+    return response.data;
+}
+
+export const updateRecipe = async (recipe) => {
+    const response = await axios.put(`${RECIPE_API}/${recipe._id}`, recipe)
+    return recipe;
+}
