df475bc3058dc174066f4b360a22a82c8a48ea2f
Ryan <ryanlooney58@gmail.com>
2022-12-08 12:19:54 -0500

    poking around and making some commenting changes

--- src/components/domain-components/post-components/RecipeJournal/index.js
+++ src/components/domain-components/post-components/RecipeJournal/index.js
@@ -19,83 +19,82 @@ const RecipeJournal = () => {
     // todo: add dynamic rendering of comments box
     return(
         <>
+        <div className="card mb-4">
 
-            <div className="card mb-4">
-
-                {/*recipe journal's title*/}
-                <div className="card-header">
-                    <span className="fw-bold fs-5">Post Title</span>
-                </div>
-
-                {/* this card body houses the entire post content */}
-                <div className="card-body">
-
-                    {/*this div houses the user's image, data and post metadata that appear in the post*/}
-                    <div className="d-flex">
-
-                        {/*user image on the recipe journal*/}
-                        <div className="mr-auto">
-                            <img className="rounded-circle"
-                                 height="70"
-                                 width={"70"}
-                                 src={imageSource}/>
-                        </div>
-
-                        {/* Rest of the post content sits within a flex to the right of the profile
-                         picture information*/}
-                        <div className="flex-grow-1 fs-6 ms-3">
-                            {/*user information in the card*/}
-                            <div className="pt-2">{userName}</div>
-                            <div className="text-secondary">{userName} - <span>{date}</span></div>
-                        </div>
-                    </div>
-
-                    {/*this div houses all post content */}
-                    <div className={"ps-4 pe-4"}>
-
-                        {/*user post text*/}
-                        <p className="mt-3">{postText}</p>
-
-                        {/*recipe information for recipe related to the post*/}
-                        <div className="border rounded py-3 px-3">
-                            <AbridgedDomainItem/>
-                        </div>
-
-                        {/*comment and like buttons, and stats */}
-                        <div className={'pt-4 ps-2 pe-2'}>
-                            {/*todo: format this text to be significantly smaller*/}
-                            {/*todo: Make the likes section a link to the list of people who liked this post*/}
-                            <span>0 likes</span>
-                            <span>&ensp;|&ensp;</span>
-                            <span>10 comments</span>
-
-                            {/* comment and like buttons */}
-                            <div className="float-end">
-                                <button className="btn">
-                                    <FontAwesomeIcon icon={regular('thumbs-up')}/>
-                                </button>
-
-                                {/*button - toggle the comments section*/}
-                                <button className="btn ms-2"
-                                        onClick={() => setCommenting(!commenting)}>
-                                    <FontAwesomeIcon icon={regular('comment')}/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {/* dynamically rendered comment section and comment entry */}
-                {commenting &&
-                    <>
-                        {/*list of comments for this post*/}
-                        <CommentsSection/>
-
-                        {/*comment entry section*/}
-                        <CommentEntry imageSource={imageSource}/>
-                    </>
-                }
+            {/*recipe journal's title*/}
+            <div className="card-header">
+                <span className="fw-bold fs-5">Post Title</span>
             </div>
+
+            {/* this card body houses the entire post content */}
+            <div className="card-body">
+
+                {/*this div houses the user's image, data and post metadata that appear in the post*/}
+                <div className="d-flex">
+
+                    {/*user image on the recipe journal*/}
+                    <div className="mr-auto">
+                        <img className="rounded-circle"
+                             height="70"
+                             width={"70"}
+                             src={imageSource}/>
+                    </div>
+
+                    {/* Rest of the post content sits within a flex to the right of the profile
+                     picture information*/}
+                    <div className="flex-grow-1 fs-6 ms-3">
+                        {/*user information in the card*/}
+                        <div className="pt-2">{userName}</div>
+                        <div className="text-secondary">{userName} - <span>{date}</span></div>
+                    </div>
+                </div>
+
+                {/*this div houses all post content */}
+                <div className={"ps-4 pe-4"}>
+
+                    {/*user post text*/}
+                    <p className="mt-3">{postText}</p>
+
+                    {/*recipe information for recipe related to the post*/}
+                    <div className="border rounded py-3 px-3">
+                        <AbridgedDomainItem/>
+                    </div>
+
+                    {/*comment and like buttons, and stats */}
+                    <div className={'pt-4 ps-2 pe-2'}>
+                        {/*todo: format this text to be significantly smaller*/}
+                        {/*todo: Make the likes section a link to the list of people who liked this post*/}
+                        <span>0 likes</span>
+                        <span>&ensp;|&ensp;</span>
+                        <span>10 comments</span>
+
+                        {/* comment and like buttons */}
+                        <div className="float-end">
+                            <button className="btn">
+                                <FontAwesomeIcon icon={regular('thumbs-up')}/>
+                            </button>
+
+                            {/*button - toggle the comments section*/}
+                            <button className="btn ms-2"
+                                    onClick={() => setCommenting(!commenting)}>
+                                <FontAwesomeIcon icon={regular('comment')}/>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* dynamically rendered comment section and comment entry */}
+            {commenting &&
+                <>
+                    {/*list of comments for this post*/}
+                    <CommentsSection/>
+
+                    {/*comment entry section*/}
+                    <CommentEntry imageSource={imageSource}/>
+                </>
+            }
+        </div>
         </>
     );
 }
