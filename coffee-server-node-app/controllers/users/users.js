da11a732f9b65242cf2cd48bc0777e23d0f1c0b7
buithuytien <buithuytien13.13@gmail.com>
2022-12-09 21:07:46 -0500

    delete server node app folder in react app

--- coffee-server-node-app/controllers/users/users.js
+++
@@ -1,6 +0,0 @@
-export default [
-    { "username": "louis", "password": "password123" },
-    { "username": "thuytien", "password": "password234"  },
-    { "username": "ryan", "password": "password345"  },
-    { "username": "vish", "password": "password456"  }
-]
